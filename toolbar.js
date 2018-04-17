8ec5139837571fc3b6091f1d0014b09ee3ff6ffa
liuxw <986503713@qq.com>
2018-04-17 22:55:09 +0800

    新增: 添加画笔

--- toolbar.js
+++ toolbar.js
@@ -1,1 +1,19 @@
 var toolList = $('#toolbar-list')
+
+window.onload = function () {
+  var canvas = document.getElementById('canvas')
+  var ctx = canvas.getContext("2d")
+  canvas.onmousedown = function (ev) {
+    var ev = ev || window.event
+    ctx.moveTo(ev.clientX - canvas.offsetLeft, ev.clientY - canvas.offsetTop)
+    document.onmousemove = function (ev) {
+      var ev = ev || window.event
+      ctx.lineTo(ev.clientX - canvas.offsetLeft, ev.clientY - canvas.offsetTop)
+      ctx.stroke()
+    }
+    document.onmouseup = function () {
+      document.onmousemove = null
+      document.onmouseup = null
+    }
+  }
+}
